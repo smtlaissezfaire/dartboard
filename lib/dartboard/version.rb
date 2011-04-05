540407601c6f94cadf90979ae97c8f605af08aeb
Scott Taylor <scott@railsnewbie.com>
2011-04-05 17:19:19 -0400

    set version to 0.1.0

--- lib/dartboard/version.rb
+++ lib/dartboard/version.rb
@@ -1,7 +1,7 @@
 module Dartboard
   module Version
     MAJOR = 0
-    MINOR = 0
+    MINOR = 1
     TINY  = 0
 
     STRING = "#{MAJOR}.#{MINOR}.#{TINY}"
--- spec/dartboard/version_spec.rb
+++ spec/dartboard/version_spec.rb
@@ -2,16 +2,16 @@ require "spec_helper"
 
 describe Dartboard do
   describe "VERSION" do
-    it "should be at 0.0.0" do
-      Dartboard::Version::STRING.should == "0.0.0"
+    it "should be at 0.1.0" do
+      Dartboard::Version::STRING.should == "0.1.0"
     end
 
     it "should have major as 0" do
       Dartboard::Version::MAJOR.should == 0
     end
 
-    it "should have minor as 0" do
-      Dartboard::Version::MINOR.should == 0
+    it "should have minor as 1" do
+      Dartboard::Version::MINOR.should == 1
     end
 
     it "should have tiny as 0" do
